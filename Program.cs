51d46086f03fc39da27af46ded8cf4927e59380a
Π19028 <nikoletaxvs@gmail.com>
2021-10-22 20:36:12 +0300

    the files

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace challenge2
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] names = { "Ryan", "Kieran", "Mark", "Jimmy" };
+            IEnumerable<string> names2 =FriendOrFoe(names);
+            foreach (string s in names2) {
+                Console.WriteLine(s);
+            } 
+        }
+        public static IEnumerable<string> FriendOrFoe(string[] names)
+        {
+            IEnumerable<string> names2 = Array.FindAll(names,x=>x.Length ==4);
+           
+            return names2;
+        }
+    }
+}
